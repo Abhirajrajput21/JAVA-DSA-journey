91a6ad672a0d580c10748512e37315dddcb03f89
Abhirajrajput21 <abhirajrajput774@gmail.com>
2026-06-21 17:24:04 +0530

    Added Two Sum solution using HashMap

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                System.out.println(map.get(complement) + " " + i);
+                return;
+            }
+
+            map.put(arr[i], i);
+        }
+
+        System.out.println("No Pair Found");
+
+        sc.close();
+    }
+}
