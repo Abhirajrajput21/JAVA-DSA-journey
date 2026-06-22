aa8e6938f2137859f42948373c6c17ab66b266a7
Abhirajrajput21 <abhirajrajput774@gmail.com>
2026-06-22 08:54:31 +0530

    Added stock buy and sell profit optimization problem

---
+++ Arrays/StockBuyAndSell.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class StockBuyAndSell {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i = 1; i < n; i++) {
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+
+        System.out.println(maxProfit);
+
+        sc.close();
+    }
+}
