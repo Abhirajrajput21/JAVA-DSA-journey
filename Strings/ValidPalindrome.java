be8d323f41e13682c037f2f835dcf379e0a07124
Abhirajrajput21 <abhirajrajput774@gmail.com>
2026-06-29 18:12:15 +0530

    Added ValidPalindrome.java solution

---
+++ Strings/ValidPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println(isPalindrome ? "Palindrome" : "Not Palindrome");
+
+        sc.close();
+    }
+}
