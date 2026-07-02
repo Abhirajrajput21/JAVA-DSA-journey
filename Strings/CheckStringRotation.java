fcd3eb5b43b18ed91044ed14d9319ac2aec8a38d
Abhirajrajput21 <abhirajrajput774@gmail.com>
2026-07-02 17:55:44 +0530

    Added CheckStringRotation.java solution

---
+++ Strings/CheckStringRotation.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CheckStringRotation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Rotation");
+        } else {
+            String temp = str1 + str1;
+
+            if (temp.contains(str2)) {
+                System.out.println("Rotation");
+            } else {
+                System.out.println("Not Rotation");
+            }
+        }
+
+        sc.close();
+    }
+}
