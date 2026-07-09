0ed858e8d6b22e3af8751dd4699137b88ba78b82
Abhirajrajput21 <abhirajrajput774@gmail.com>
2026-07-09 10:29:21 +0530

    Added search insert position using binary search

---
+++ Searching-and-Sorting/SearchInsertPosition.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        int left = 0, right = n - 1;
+        int ans = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
